26d1c17351a8a5805cf6cdb436ffd5a188180265
teguayco <alu0100825503@ull.edu.es>
2016-11-18 16:36:41 +0200

    añadiendo localstorage al login

--- platforms/browser/www/js/login.js
+++ platforms/browser/www/js/login.js
@@ -2,6 +2,15 @@ $(document).ready(function() {
 	//url = "http://socialcalendarplus.esy.es/login.php";
 	url = "http://localhost/login.php";
 
+	console.log(window.localStorage);
+
+	// If the browser supports localStorage and we have some stored data
+	if (window.localStorage && localStorage.username) {
+		console.log("stored user: " + localStorage.username);
+		$("#usernamelogin").val(localStorage.username);
+		$("#passwordlogin").val(localStorage.password);	
+	}
+
 	$("#submitlogin").click(function(evt) {
 		evt.preventDefault();
 		formData = {
@@ -11,19 +20,25 @@ $(document).ready(function() {
 
 		//console.log(formData);
 		var ajaxRequest = $.post(url, formData, function(returnedData) {
-			console.log(returnedData);
 			if (returnedData.success) {
+				// Store login data for a valid user
+				if ($('#rememberme').is(":checked")) {
+					if (window.localStorage) {
+						console.log("Web browser supports LocalStorage");
+						localStorage.setItem("username", $("#usernamelogin").val());
+						localStorage.password = $("#passwordlogin").val();
+					}
+				};
 				window.location = "profile.html";
 			}
 			else {
-				// Show a warning popup
-				console.log("failed");
-				$("#popupLogin").popup("close");
+				console.log("login failed");
+				// Show warning popup:
 				$("#popupDialogLogin").popup("open");
 			}
 		}, 'json')
 		.fail(function() {
-			console.log("error en la respuesta del servidor");
+			console.log("server connection failed");
 		});
 	});
 });
