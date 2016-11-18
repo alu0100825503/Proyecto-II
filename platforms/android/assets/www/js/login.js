242ecc7ef755c312c209e490ebe8bedff196cdf7
teguayco <alu0100825503@ull.edu.es>
2016-11-18 16:47:53 +0200

    mejorando localStorage en rememberme en index.html

--- platforms/android/assets/www/js/login.js
+++ platforms/android/assets/www/js/login.js
@@ -1,34 +1,22 @@
-$(document).ready(function() {
+function makePostRequest(dataForServer) {
 	//url = "http://socialcalendarplus.esy.es/login.php";
 	url = "http://localhost/login.php";
 
-	console.log(window.localStorage);
-
-	// If the browser supports localStorage and we have some stored data
-	if (window.localStorage && localStorage.username) {
-		console.log("stored user: " + localStorage.username);
-		$("#usernamelogin").val(localStorage.username);
-		$("#passwordlogin").val(localStorage.password);	
-	}
-
-	$("#submitlogin").click(function(evt) {
-		evt.preventDefault();
-		formData = {
-			"username": $("#usernamelogin").val(),
-			"password": $("#passwordlogin").val()
-		}
-
-		//console.log(formData);
-		var ajaxRequest = $.post(url, formData, function(returnedData) {
+	var ajaxRequest = $.post(url, dataForServer, function(returnedData) {
 			if (returnedData.success) {
-				// Store login data for a valid user
+				// Store login data for a valid user if remember me is checked
 				if ($('#rememberme').is(":checked")) {
 					if (window.localStorage) {
 						console.log("Web browser supports LocalStorage");
 						localStorage.setItem("username", $("#usernamelogin").val());
 						localStorage.password = $("#passwordlogin").val();
 					}
-				};
+				} 
+				else {
+					// Delete login data if remember me is unchecked
+					localStorage.removeItem("username");
+					localStorage.removeItem("password");
+				}
 				window.location = "profile.html";
 			}
 			else {
@@ -40,5 +28,24 @@ $(document).ready(function() {
 		.fail(function() {
 			console.log("server connection failed");
 		});
+}
+
+$(document).ready(function() {
+	// If the browser supports localStorage and we have some stored data
+	if (window.localStorage && localStorage.username) {
+		console.log("stored user: " + localStorage.username);
+		$("#usernamelogin").val(localStorage.username);
+		$("#passwordlogin").val(localStorage.password);
+		// TODO: hacer inicio de sesión automático si hay datos 
+		// guardados en localStorage
+	}
+
+	$("#submitlogin").click(function(evt) {
+		evt.preventDefault();
+		formData = {
+			"username": $("#usernamelogin").val(),
+			"password": $("#passwordlogin").val()
+		}
+		makePostRequest(formData);
 	});
 });
