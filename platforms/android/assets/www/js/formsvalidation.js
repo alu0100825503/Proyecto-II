4f3a98e7230a2fc4116d29c890f1e781e9a63987
teguayco <alu0100825503@ull.edu.es>
2016-11-19 11:02:19 +0200

    funcionando los avisos de error en formulario de registro

--- platforms/android/assets/www/js/formsvalidation.js
+++ platforms/android/assets/www/js/formsvalidation.js
@@ -9,48 +9,81 @@ $(document).ready(function() {
 		var lastname = $("#userlastname").val();
 		var birth = $("#date").val();
 		var email = $("#email").val();
-		var tel = $("#telephone").val();
+		var tel = $("#tel").val();
 		var error = false;
 		
-		console.log("realname: " + realname);
-		console.log("lastname: " + lastname);
-		
+		// Limpiar campos inicialmente
+		$("#newusername").parent().css('border-color', 'green');
+		$("#password").parent().css('border-color', 'green');
+		$("#pwrepeat").parent().css('border-color', 'green');
+		$("#userrealname").parent().css('border-color', 'green');
+		$("#userlastname").parent().css('border-color', 'green');
+		$("#date").parent().css('border-color', 'green');
+		$("#email").parent().css('border-color', 'green');
+		$("#tel").parent().css('border-color', 'green');
+		$("#warningusername").text("");
+		$("#warningpassword").text("");
+		$("#warningname").text("");
+		$("#warninglastname").text("");
+		$("#warningdate").text("");
+		$("#warningtel").text("");
+		$("#warningtel").text("");
+
+		if (username == "") {
+			$("#newusername").parent().css('border-color', 'red');
+			$("#warningusername").text("Debe indicar un nombre de usuario.");
+			error = true;
+		}
+
 		// Las contraseñas introducidas deben ser iguales
 		if (password !== "" && rptpassword !== "") {
 			if (password !== rptpassword) {
-				error = true;
 				// Set textboxes red
 				$("#password").parent().css('border-color', 'red');
 				$("#pwrepeat").parent().css('border-color', 'red');
+				$("#warningpassword").text("Las contraseñas no coinciden.");
+				error = true;
 			}	
 		} else {
-			error = true;
 			$("#password").parent().css('border-color', 'red');
 			$("#pwrepeat").parent().css('border-color', 'red');
+			$("#warningpassword").text("Rellene ambos campos de contraseña.");
+			error = true;
 		}
 
-		// El nombre y apellidos del usuario deben estar formados únicamente letras
-		if (!realname.match(/^[a-zA-Z]+$/) || !lastname.match(/^[a-zA-Z]+$/)) {
-			error = true;
+		// El nombre debe estar compuesto únicamente por caracteres alfabéticos
+		if (!realname.match(/^[a-zA-Z]+$/)) {
 			$("#userrealname").parent().css('border-color', 'red');
+			$("#warningname").text("El nombre sólo puede contener letras");
+			error = true;
+		} 
+		
+		// El apellido debe estar compuesto únicamente por caracteres alfabéticos
+		if (!lastname.match(/^[a-zA-Z]+$/)) {
 			$("#userlastname").parent().css('border-color', 'red');
+			$("#warninglastname").text("Los apellidos sólo pueden contener letras.");
+			error = true;
 		}
 		
 		// La fecha de nacimiento debe ser una fecha válida
 		if (birth == "") {
-			error = true;
 			$("#date").parent().css('border-color', 'red');
+			$("#warningdate").text("Debe indicar un fecha de nacimiento.");
+			error = true;
 		}
 		
  		// El email debe tener el formato correcto, dada por la re
 		if (!re_email.test(email)) {
-			alert("error en email");
+			$("#email").parent().css('border-color', 'red');
+			$("#warningemail").text("Formato de correo no válido.");
+			error = true;
 		}
 
 		// El télefono debe estar compuesto por 9 caracteres numéricos
 		if (!tel.match(/^[\d]{9}/)) {
+			$("#tel").parent().css('border-color', 'red');
+			$("#warningtel").text("El número de teléfono no es correcto.");
 			error = true;
-			alert("error en tel");
 		}
 
 		// Mostrar los posibles errores
@@ -73,15 +106,22 @@ $(document).ready(function() {
 				console.log(returnedData);
 				if (returnedData.success) {
 					// éxito: llevar al usuario a profile.html por ejemplo
+					// popup informando del éxito en el registro
+					// guardar en el objeto de sesión al usuario
 				} else {
 					if (returnedData.userAlreadyExists) {
 						console.log("user already exists in the db");
+						$("#newusername").parent().css('border-color', 'red');
+						$("#warningusername").text("El nombre de usuario ya existe.");
 					}
 					else if (returnedData.emailAlreadyExists) {
 						console.log("email already exists in the db");
+						$("#email").parent().css('border-color', 'red');
+						$("#warningemail").text("El nombre de usuario ya existe.");
 					}
 					else {
 						console.log("unknown error");
+						alert("Ooooops! Error inesperado. Inténtelo de nuevo.");
 					}
 				}
 			}, 'json')
--- platforms/browser/www/php/register.php
+++ platforms/browser/www/php/register.php
@@ -1,10 +1,12 @@
 <?php
-session_start();
+header('content-type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
 
 /*
 $servername = "localhost";
 $username = "root";
-$dbname = "diary_db";
+$password = "";
+$dbname = "u344358176_calen";
 */
 
 $servername = "mysql.hostinger.es";
@@ -17,38 +19,73 @@ $form_password = $_POST['pass'];
 $form_name = $_POST['realname'];
 $form_lastname = $_POST['lastname'];
 $form_date = $_POST['date'];
+$form_email = $_POST['email'];
+$form_tel = $_POST['tel'];
 
-/*DEGUB LINES
-echo $form_username;
-echo $form_password;
-echo $form_name;
-echo $form_lastname;
-echo $form_date;
-*/
+// Objeto JSON que contendrá la respuesta para el cliente
+$json_response = array();
 
 // Create connection
-if(!($id = mysqli_connect($servername, $username, $password))) { 
+if(!($connection = mysqli_connect($servername, $username, $password))) { 
     die("Error: Could not connect");
 }
 
 // Select DB 
-if(!mysqli_select_db($id, $dbname)) {
+if(!mysqli_select_db($connection, $dbname)) {
     die("Error: DB does not exist");
 }
 
-$sql_insert = "INSERT INTO User VALUES ".
-    "('".$form_username."','".
-    $form_name."','".
-    $form_lastname."','".
-    $form_password."','".
-    $form_date."');";
+// Comprobar primero que no existe el nombre de usuario en la base de datos
+$sql_select_username = "SELECT NAME FROM User".
+    " WHERE username='$form_username';";
 
-echo $_SESSION['userid'];
+$result_name = mysqli_query($connection, $sql_select_username);
 
-if (!mysqli_query($id, $sql_insert)) {
-    die("Error: Could not execute the query");
+if ($result_name) {
+    $json_response['success'] = false;
+    $json_response['userAlreadyExists'] = true;
+}
+else {
+    // Comprobar ahora que no existe el correo electrónico
+    $sql_select_email = "SELECT NAME FROM User".
+        " WHERE email='$form_email';";
+
+    $result_email = mysqli_query($connection, $sql_select_email);
+
+    if ($result_email) {
+        $json_response['success'] = false;
+        $json_response['emailAlreadyExists'] = true;
+    } else {
+        // Insertamos finalmente al nuevo usuario si no existe el nombre de usuario
+        // y correo electrónico insertados
+        $sql_insert = "INSERT INTO User VALUES ".
+        "('".$form_username."','".
+        $form_name."','".
+        $form_lastname."','".
+        $form_password."','".
+        $form_date."','".
+        $form_email."','".
+        $form_tel."');";
+
+        $result = mysqli_query($connection, $sql_insert);
+
+        if ($result) {
+            $json_response['success'] = true;
+            $json_response['username'] = $form_username;
+            $json_response['password'] = $form_password;
+        } else {
+            $json_response['success'] = false;
+        }
+    }
 }
 
+// Envío del objeto JSON como respuesta
+header('Content-type: application/json; charset=utf-8');
+echo json_encode($json_response, JSON_FORCE_OBJECT);
+
+// Set the result memory free
+mysqli_free_result($result);
+
 // Close connection 
-mysqli_close($id); 
+mysqli_close($connection); 
 ?>
