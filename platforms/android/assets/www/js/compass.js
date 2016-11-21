a82a8af299508fde30766902e1b9122af7d04f92
teguayco <alu0100825503@ull.edu.es>
2016-11-21 20:31:16 +0200

    acabada pantalla para la brújula

--- platforms/android/assets/www/js/compass.js
+++ platforms/android/assets/www/js/compass.js
@@ -1,53 +1,48 @@
-$(document).ready(function() {
+// PhoneGap is ready
+function onDeviceReady() {
+    startWatch();
+}
 
-	
-	// The watch id references the current `watchHeading`
+// Start watching the compass
+function startWatch() {
+    // Update compass every 1 seconds
+    var options = { frequency: 1000 };
+    watchID = navigator.compass.watchHeading(onSuccess, onError, options);
+}
+
+// Stop watching the compass
+function stopWatch() {
+    if (watchID) {
+        navigator.compass.clearWatch(watchID);
+        watchID = null;
+    }
+}
+
+// onSuccess: Get the current heading
+function onSuccess(heading) {
+    $("#measureVal").text(heading.trueHeading);
+    $("#measure").text("GRADOS");
+}
+
+// onError: Failed to get the heading
+function onError() {
+    alert('onError!');
+}
+
+$(document).ready(function() {
+    // The watch id references the current `watchHeading`
     var watchID = null;
 
-	console.log("compass");
-
-    // Wait for PhoneGap to load
-    //
-    document.addEventListener("deviceready", onDeviceReady, false);
-
-    // PhoneGap is ready
-    //
-    function onDeviceReady() {
-        startWatch();
-    }
-
-    // Start watching the compass
-    //
-    function startWatch() {
-		console.log("start");
-        // Update compass every 3 seconds
-        var options = { frequency: 2000 };
-
-		console.log(navigator.compass);
-        //watchID = navigator.compass.watchHeading(onSuccess, onError, options);
-    }
-
-    // Stop watching the compass
-    //
-    function stopWatch() {
-        if (watchID) {
-            navigator.compass.clearWatch(watchID);
-            watchID = null;
+    $("#compassButton").click(function() {
+        if ($(this).val() == "Medir") {
+            $(this).val("Detener");
+            $(this).button("refresh");
+            document.addEventListener("deviceready", onDeviceReady, false);
+        } 
+        else {
+            $(this).val("Medir");
+            $(this).button("refresh");
+            stopWatch();
         }
-    }
-
-    // onSuccess: Get the current heading
-    //
-    function onSuccess(heading) {
-		console.log(heading);
-    }
-
-    // onError: Failed to get the heading
-    //
-    function onError() {
-        alert('onError!');
-    }
-
-	startWatch();
-	
+    }); 
 });
