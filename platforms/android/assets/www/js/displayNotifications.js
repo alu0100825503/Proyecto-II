076f325a7454c0bb89693670209e13c46fe4dd50
teguayco <alu0100825503@ull.edu.es>
2016-11-24 18:50:16 +0200

    cambiando a tema b en notifications.html

--- platforms/android/assets/www/js/displayNotifications.js
+++ platforms/android/assets/www/js/displayNotifications.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
-	console.log("displaying notifications...");
+	console.log("displaying notifications....");
 
 	console.log(localStorage.notifications);
 	notifications_obj = JSON.parse(localStorage.notifications);
 
+	messagesSection = $("#messagesCollapsible");
+	eventsSection = $("#eventsCollapsible");
+	contactsSection = $("#contactsCollapsible");
+
 	jQuery.each(notifications_obj.notifications, function(i, val) {
 		if (val.type == "message") {
 			console.log("tiene mensajes");
-		
+			var newMessageButton = $('<a class="ui-btn ui-btn-b ui-icon-delete ui-btn-icon-left">' + val.sender + ": <i>" + val.message_subject + "</i></a>");
+			$("#messagesContainer").append(newMessageButton);
+			//messagesSection.collapsible();
+
 		} else if (val.type == "invitation") {
 			console.log("tiene solicitudes de eventos");
 
--- www/js/notifications.js
+++ www/js/notifications.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
 			if (returnedData.success) {
 				// Guardar el objeto json de notificaciones en
 				// localStorage para hacerlo accesible a notifications.html
+				console.log("returnedData: " + returnedData);
 				localStorage.notifications = JSON.stringify(returnedData);
 				// Poner el número de notificaciones sobre el botón de notificaciones	
 				// en rojo
