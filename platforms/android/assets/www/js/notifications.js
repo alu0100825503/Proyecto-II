9f5c119a01b2cf22a6b408148e58a5d93a737d34
teguayco <alu0100825503@ull.edu.es>
2016-11-24 16:10:07 +0200

    getNotifications.php devolviendo correctamente json de notificaciones

--- platforms/android/assets/www/js/notifications.js
+++ platforms/android/assets/www/js/notifications.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
 	console.log("notifications");
-	
+
+	var url = "http://socialcalendarplus.esy.es/getNotifications.php";
+	var receiver = localStorage.username;
+	console.log("retrieving info for " + receiver);
+	dataForServer = { "receiver": receiver };
+
+	var ajaxRequest = $.post(url, dataForServer, function(returnedData) {
+			if (returnedData.success) {
+				console.log(returnedData);
+			}
+			else {
+				console.log("not success in the json");
+				console.log(returnedData);
+			}	
+		}, 'html')
+		.fail(function() {
+			console.log("the response from the server failed");
+		});
 });
--- platforms/browser/www/php/notifications.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('content-type: application/json; charset=utf-8');
-header('Access-Control-Allow-Origin: *');
-
-
-
-
-
-?>
