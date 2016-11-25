e434ab7a0a0843dc6389065ce291dc1b2e3d8dac
teguayco <alu0100825503@ull.edu.es>
2016-11-25 21:09:46 +0200

    implementada la visualización de mensajes desde notificaciones

--- www/js/displayNotifications.js
+++ www/js/displayNotifications.js
@@ -1,25 +1,53 @@
-function notificationButtonHandler(notifications) {
+function notificationButtonHandler(event) {
 	url = "http://socialcalendarplus.esy.es/updateReadMessages.php";
-	/*
+
+	var buttonClickedId = this.id;
+	var notIndex = buttonClickedId.match(/\d+/g);
+	var notification = event.data.notification;
+	console.log(notification);
+	
 	dataForServer = {
-		"sender": notifications[],
-		"receiver": ,
-		"date": 
+		"sender": notification.sender,
+		"receiver": notification.receiver,
+		"date": notification.date
 	}
 
+	// Remove color green from the button clicked
+	$(this).css("background-color", "");
+
+	// Marcar como leída en la base de datos
 	var ajaxRequest = $.post(url, dataForServer, function(returnedData) {
 			console.log(returnedData);
 			if (returnedData.success) {
-
+				console.log();
 			}
 			else {
-
+				console.log("update did not success");
+				console.log("returnedData: " + returnedData);	
 			}
 		}, 'json')
 		.fail(function() {
 			console.log("server connection failed");
 		});
-		*/
+
+	// TODO Mostrar popup sobre la notificación
+	// ......
+
+	if (notification.type == "message") {
+		$("#messageSubject").empty();
+		$("#messageDate").empty();
+		$("#messageFrom").empty();
+		$("#messageTo").empty();
+		$("#messageText").empty();
+
+		$("#messageSubject").append("<strong>Asunto: </strong>" + notification.message_subject);
+		$("#messageDate").append("<strong>Fecha: </strong>" + notification.date);
+		$("#messageFrom").append("<strong>De: </strong>" + notification.sender);
+		$("#messageTo").append("<strong>Para: </strong>" + notification.receiver);
+		$("#messageText").append("<i>" + notification.message_content + "</i>");
+		$("#messageViewer").popup();
+		$("#messageViewer").popup("open");	
+	}
 };
 
 $(document).ready(function() {
@@ -61,7 +89,6 @@ $(document).ready(function() {
 				val.sender + ": <i>" +
 				val.message_subject +
 				"</i></a>");
-			newMessageButton.click(notificationButtonHandler(notifications));
 
 			// Si el mensaje no ha sido leído, se pone primero en la lista	
 			if (val.is_read > 0) {
@@ -93,4 +120,9 @@ $(document).ready(function() {
 			console.log("notification type not supported");
 		}
 	});
+
+	// Setting listener for all notifications buttons
+	for (i = 0; i < notifications.length; i++) { 
+    	$("#not" + i).click({ notification: notifications[i] }, notificationButtonHandler);
+	}
 });
