d02d5810ff7c7d7f218428a50d4f3d8221e3b0b3
teguayco <alu0100825503@ull.edu.es>
2016-11-26 12:45:41 +0200

    eliminando líneas de debug en js de notificaciones

--- platforms/android/assets/www/js/displayNotifications.js
+++ platforms/android/assets/www/js/displayNotifications.js
@@ -17,7 +17,6 @@ function notificationButtonHandler(event) {
 
 	// Marcar como leída en la base de datos
 	var ajaxRequest = $.post(url, dataForServer, function(returnedData) {
-			console.log(returnedData);
 			if (returnedData.success) {
 				console.log();
 			}
@@ -30,9 +29,7 @@ function notificationButtonHandler(event) {
 			console.log("server connection failed");
 		});
 
-	// TODO Mostrar popup sobre la notificación
-	// ......
-
+	// Categorizar las notificaciones que nos llegan del servidor	
 	if (notification.type == "message") {
 		$("#messageSubject").empty();
 		$("#messageDate").empty();
@@ -56,9 +53,6 @@ function notificationButtonHandler(event) {
 };
 
 $(document).ready(function() {
-	console.log("displaying notifications....");
-
-	console.log(localStorage.notifications);
 	notifications_obj = JSON.parse(localStorage.notifications);
 
 	var n_messages = 0, 
@@ -90,7 +84,6 @@ $(document).ready(function() {
 	jQuery.each(notifications_obj.notifications, function(i, val) {
 		notifications.push(val);
 		if (val.type == "message") {
-			console.log("tiene mensajes");
 			var newMessageButton = $('<a id="not' + i + '"data-icon="carat-r" class="ui-btn ui-btn-b ui-icon-carat-r ui-btn-icon-left">' + 
 				val.sender + ": <i>" +
 				val.message_subject +
@@ -109,7 +102,6 @@ $(document).ready(function() {
 			console.log("tiene solicitudes de eventos");
 
 		} else if (val.type == "friendship") {
-			console.log("tiene solicitudes de contacto");
 			var newContactButton = $('<a id="not' + i + '"data-icon="carat-r" class="ui-btn ui-btn-b ui-icon-carat-r ui-btn-icon-left">' + 
 				val.sender + ": <i>" +
 				val.message_subject +
--- www/js/notifications.js
+++ www/js/notifications.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
-	console.log("notifications");
-
 	var url = "http://socialcalendarplus.esy.es/getNotifications.php";
 	var receiver = localStorage.username;
-	console.log("retrieving info for " + receiver);
 	dataForServer = { "receiver": receiver };
 
 	var ajaxRequest = $.post(url, dataForServer, function(returnedData) {
@@ -11,7 +8,7 @@ $(document).ready(function() {
 				// Guardar el objeto json de notificaciones en
 				// localStorage para hacerlo accesible a diplayNotifications.js
 				localStorage.notifications = JSON.stringify(returnedData);
-				console.log("returnedData in success: " + JSON.stringify(returnedData));
+				
 				// Contar notificaciones no leídas
 				var newNotifications = 0;
 				jQuery.each(returnedData.notifications, function(i, val) {
@@ -26,7 +23,7 @@ $(document).ready(function() {
 					$("#notificationsbutton").css("-moz-border-radius", "10px");
 					$("#notificationsbutton").css("-webkit-border-radius", "100px");
 					$("#notificationsbutton").css("border-radius", "100px");
-					//console.log("Nuevas notificaciones: " + newNotifications);
+					console.log("Nuevas notificaciones: " + newNotifications);
 				}
 			}
 			else {
