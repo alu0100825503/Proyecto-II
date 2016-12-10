45571396748aa8f203f1dc468ad5cd10c476bdfc
Rubén <alu0100309553@ull.edu.es>
2016-12-10 12:36:02 +0000

    Trabajando en subir archivos

---
+++ platforms/android/assets/www/js/filesetter.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $("#form").on('submit',function () {
+    var formData = new FormData(this);
+    $.ajax({url:"http://socialcalendarplus.esy.es/fileSetter.php",
+    type: "POST",
+    data: formData,
+    contentType: false,
+    cache: false,
+    processData:false,
+    success: function(data){
+      console.log("entro");
+      console.log(data.success);
+    },
+    error: function(){
+      console.log("ha fallado");
+    }});
+
+    //https://www.html5rocks.com/es/tutorials/file/dndfiles/
+    //https://abandon.ie/notebook/simple-file-uploads-using-jquery-ajax
+    //http://casamadrugada.net/tutoriales/php/como-almacenar-archivos-imagenes-en-mysql-utilizando-php/
+    //https://www.formget.com/ajax-image-upload-php/
+    //http://www.aorank.com/tutorial/Live_demo_ajax_upload_image/ajax_upload_image_main.php
+    //https://manuais.iessanclemente.net/index.php/Almacenamiento_de_im%C3%A1genes_en_bases_de_datos_con_PHP#Formulario_para_subir_im.C3.A1genes
+
+
+  });
+});
---
+++ www/php/filesetter.php
@@ -0,0 +1,38 @@
+<?php
+
+header('content-type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+$jsondata = array();
+
+if(count($_FILES) > 0) {
+  if(is_uploaded_file($_FILES['userImage']['tmp_name'])) {
+    $dbserver = "mysql.hostinger.es";
+    $dbuser = "u344358176_calen";
+    $password = "supercalendar";
+    $dbname = "u344358176_calen";
+    $database = new mysqli($dbserver, $dbuser, $password, $dbname);
+    if($database->connect_errno) {
+      die("No se pudo conectar a la base de datos");
+    }
+    $imgData =addslashes(file_get_contents($_FILES['file']['tmp_name']));
+    $imageProperties = getimageSize($_FILES['file']['tmp_name']);
+
+    if ( $database->query( "INSERT INTO Files (name, size, data)
+    VALUES('$_FILES['file']['tmp_name']', '$imageProperties', '$imgData')") ){
+      $jsondata["success"] = true;
+      $jsondata["data"] = array('message' => "Actualización correcta '$data'" );
+    } else {
+      $jsondata["success"] = false;
+      $jsondata["data"] = array('message' => $database->error);
+    }
+  }
+}
+
+
+  header('Content-type: application/json; charset=utf-8');
+  echo json_encode($jsondata, JSON_FORCE_OBJECT);
+  $database->close();
+}
+exit();
+
+?>
